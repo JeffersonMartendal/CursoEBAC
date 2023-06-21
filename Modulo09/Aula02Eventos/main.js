3f1d2eebbe546835b2a94665bc246870994e3168
Jefferson Martendal de Barros <55723528+JeffersonMartendal@users.noreply.github.com>
2023-06-21 12:43:13 -0300

    Acrescentando aula

---
+++ Modulo09/Aula02Eventos/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('header button').click(function() {
+        alert("Expandir formulário")
+    })
+
+    $('form').on('submit', function(e) {
+        console.log("submit")
+        e.preventDefault()
+    })
+})
