b313554f67ac57811eea2d7f8f05633249dac5fb
Jefferson Martendal de Barros <55723528+JeffersonMartendal@users.noreply.github.com>
2023-08-06 18:41:59 -0300

    Acrescentando aula

---
+++ Modulo16/Aula03/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp')
+
+function funcaoPadrao(callback) {
+    console.log("Executando via Gulp")
+    callback()
+}
+
+function dizOi(callback) {
+    console.log("Olá Gulp")
+    dizTchau()
+    callback()
+}
+
+function dizTchau() {
+    console.log("Tchau Gulp")
+}
+
+exports.default = gulp.series(funcaoPadrao, dizOi)
+exports.dizOi = dizOi
