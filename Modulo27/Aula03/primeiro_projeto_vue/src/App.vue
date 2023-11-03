d86c0f3fff74a54700caac3b92becc646bfee13d
Jefferson Martendal de Barros <jeffersonmarten@gmail.com>
2023-11-02 22:30:04 -0300

    Alterando aula

--- Modulo27/Aula03/primeiro_projeto_vue/src/App.vue
+++ Modulo27/Aula03/primeiro_projeto_vue/src/App.vue
@@ -11,15 +11,31 @@ function dizOla(nome) {
 
 const enderecoDaImagemDoBatman = "https://i0.wp.com/www.crossovernerd.com/wp-content/uploads/2019/07/Batman_guia_leitura_cronologica.jpg?fit=1280%2C720&ssl=1"
 
+const imagemSuperman = "https://i.pinimg.com/474x/a5/4a/64/a54a640e1472797500d0c1cf2c34a711.jpg"
+
 const botaoEstaDesabilitado = false
+
+const gostaDoBatman = false
+const gostaDoSuperman = false
+
+const estaAutorizado = false
 </script>
 
 <template>
   <h1>{{ dizOla("Paula") }}</h1>
-  <img :src="enderecoDaImagemDoBatman" alt="">
+  <img v-if="gostaDoBatman" :src="enderecoDaImagemDoBatman" alt="">
+  <img v-else-if="gostaDoSuperman" :src="imagemSuperman" alt="">
+  <h2 v-else>Não curte heróis da DC</h2>
 
-  <button :disabled="!botaoEstaDesabilitado">Enviar mensagem</button>
+  <h1 v-if="estaAutorizado">Bem-vindo</h1>
+  <h1 v-else>Não possui acesso</h1>
+
+  <button :disabled="botaoEstaDesabilitado">Enviar mensagem</button>
 </template>
 
 <style scoped>
+
+img {
+  max-width: 200px;
+}
 </style>
