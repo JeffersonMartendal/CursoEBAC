195148e16c53f313f13bf13fd8f1117e0e81c1cf
Jefferson Martendal de Barros <jeffersonmarten@gmail.com>
2023-11-02 22:20:17 -0300

    Acrescentando aula

---
+++ Modulo27/Aula03/primeiro_projeto_vue/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+const nome = "Jefferson Martendal"
+const meuObj = {
+  nome: "Jefferson",
+  filmeFavorito: "Mr. Nobody"
+}
+
+function dizOla(nome) {
+  return `${nome} diz oi`
+}
+
+const enderecoDaImagemDoBatman = "https://i0.wp.com/www.crossovernerd.com/wp-content/uploads/2019/07/Batman_guia_leitura_cronologica.jpg?fit=1280%2C720&ssl=1"
+
+const botaoEstaDesabilitado = false
+</script>
+
+<template>
+  <h1>{{ dizOla("Paula") }}</h1>
+  <img :src="enderecoDaImagemDoBatman" alt="">
+
+  <button :disabled="!botaoEstaDesabilitado">Enviar mensagem</button>
+</template>
+
+<style scoped>
+</style>
