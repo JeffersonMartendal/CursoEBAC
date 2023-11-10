cd6b2684a6e14d3d862df0f4243b3856581f9942
Jefferson Martendal de Barros <jeffersonmarten@gmail.com>
2023-11-09 21:52:45 -0300

    Acrescentando aula

---
+++ Modulo28/Aula03/projeto-react/src/App.jsx
@@ -0,0 +1,13 @@
+import Perfil from "./components/Perfil"
+import Formulario from "./components/formulario"
+
+function App() {
+  return (
+    <>
+      <Perfil nome="Jefferson" endereco="https://github.com/JeffersonMartendal.png"/>
+      <Formulario />
+    </>
+  )
+}
+
+export default App
