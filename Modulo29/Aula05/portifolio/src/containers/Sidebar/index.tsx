a23627087dd35bedd1e887b594c01557158b1f5c
Jefferson Martendal de Barros <jeffersonmarten@gmail.com>
2023-11-17 20:28:01 -0300

    Acrescentando aula

---
+++ Modulo29/Aula05/portifolio/src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/JeffersonMartendal.png" />
+    <Titulo>Jefferson Martendal</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ Modulo29/Aula05/portifolio/src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque
+      exercitationem temporibus eaque voluptatibus voluptate voluptates iure
+      tempora fugiat pariatur repellendus porro itaque autem nam in, mollitia,
+      ullam sit odit ducimus.
+    </p>
+  </section>
+)
+
+export default Sobre
