ffd0af105a713c4f9894ff7e7476fb377aeccf08
Jefferson Martendal de Barros <jeffersonmarten@gmail.com>
2023-11-18 21:43:07 -0300

    Acrescentando aula

---
+++ Modulo29/Aula06/portifolio/src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafro from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafro tipo="secundario">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque
+      exercitationem temporibus eaque voluptatibus voluptate voluptates iure
+      tempora fugiat pariatur repellendus porro itaque autem nam in, mollitia,
+      ullam sit odit ducimus.
+    </Paragrafro>
+  </section>
+)
+
+export default Sobre
