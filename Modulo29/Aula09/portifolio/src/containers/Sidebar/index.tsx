a9b03d541dcc717432e98bd0eeb342be0f2f57d7
Jefferson Martendal de Barros <jeffersonmarten@gmail.com>
2023-11-21 21:52:49 -0300

    Acrescentando aula

---
+++ Modulo29/Aula09/portifolio/src/containers/Sidebar/index.tsx
@@ -0,0 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
+const Sidebar = () => (
+  <aside>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Jefferson Martendal</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        JeffersonMartendal
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </aside>
+)
+
+export default Sidebar
